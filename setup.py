3c30f40906ab558ba8c35ce34f604cd164b95166
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-12-03 07:56:42 +0100

    pliz render

--- setup.py
+++ setup.py
@@ -22,15 +22,16 @@ setuptools.setup(
   long_description_content_type = 'text/markdown',
   url = 'https://git.niklasrosenstein.com/NiklasRosenstein/pliz',
   license = 'MIT',
-  packages = setuptools.find_packages('src'),
+  packages = setuptools.find_packages('src', ['test', 'test.*', 'docs', 'docs.*']),
   package_dir = {'': 'src'},
   include_package_data = False,
   install_requires = requirements,
   tests_require = [],
   python_requires = None, # TODO: '>=2.7,<3.0.0|>=3.4,<4.0.0',
+  data_files = [],
   entry_points = {
     'console_scripts': [
       'pliz = pliz.__main__:console_main',
-    ],
+    ]
   }
 )
