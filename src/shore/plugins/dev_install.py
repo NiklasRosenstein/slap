e0c655dfb31b055971cf280c4beabfb39a13fc77
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-04-23 19:02:40 +0200

    update dev-install plugin: it now correctly references the directory name for installing with Pip rather than the package or module name

--- src/shore/plugins/dev_install.py
+++ src/shore/plugins/dev_install.py
@@ -22,13 +22,17 @@
 from pkg_resources import resource_string
 from shore import __version__
 from shore.core.plugins import FileToRender, IMonorepoPlugin
-from shore.model import Monorepo
+from shore.model import Monorepo, Package
 from nr.algo.graph.toposort import toposort
 from nr.databind.core import Field, Struct
 from nr.interface import implements, override
 from typing import Dict, Iterable
 import json
 import os
+
+
+def _dirname(package: Package):
+  return os.path.basename(package.directory)
 
 
 @implements(IMonorepoPlugin)
@@ -64,23 +68,28 @@ class DevInstallRenderer:
 
   def _get_interpackage_dependencies(self, monorepo: Monorepo) -> Dict[str, Dict]:
     nodes = {}
+    package_name_mapping = {}
+
     packages = list(monorepo.get_packages())
     for package in packages:
-      nodes[package.name] = {
+      package_name_mapping[package.name] = _dirname(package)
+      nodes[_dirname(package)] = {
         'directory': os.path.basename(package.directory),
         'requires': [],
         'extra_requires': {}
       }
 
+    print(package_name_mapping)
+
     def _flatten_reqs(dst, reqs):
       for req in reqs.required if reqs else ():
-        if req.package in nodes:
-          dst.append(req.package)
+        if req.package in package_name_mapping:
+          dst.append(package_name_mapping[req.package])
 
     for package in packages:
-      _flatten_reqs(nodes[package.name]['requires'], package.requirements)
-      _flatten_reqs(nodes[package.name].setdefault('test', []), package.requirements.test)
+      _flatten_reqs(nodes[_dirname(package)]['requires'], package.requirements)
+      _flatten_reqs(nodes[_dirname(package)].setdefault('test', []), package.requirements.test)
       for extra, reqs in package.requirements.extra.items():
-        _flatten_reqs(nodes[package.name].setdefault(extra, []), reqs)
+        _flatten_reqs(nodes[_dirname(package)].setdefault(extra, []), reqs)
 
     return nodes
