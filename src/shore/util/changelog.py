1f70df1e72d696b758e5a5bf430c7793a56f7223
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-06-06 02:26:02 +0200

    update "shore changelog" cli and add --markdown option

--- src/shore/util/changelog.py
+++ src/shore/util/changelog.py
@@ -20,9 +20,14 @@
 # IN THE SOFTWARE.
 
 from nr.databind.core import Field, ObjectMapper, Struct
+from nr.stream import Stream
 from shore.util.version import Version
-from typing import Optional
+from termcolor import colored
+from typing import Optional, TextIO
 import os
+import re
+import shutil
+import textwrap
 import yaml
 
 
@@ -34,6 +39,8 @@ class ChangelogEntry(Struct):
 
 
 class Changelog:
+
+  RENDERERS = {}
 
   def __init__(self, filename: str, version: Optional[Version], mapper: ObjectMapper) -> None:
     self.filename = filename
@@ -58,6 +65,9 @@ class Changelog:
 
   def add_entry(self, entry: ChangelogEntry) -> None:
     self.entries.append(entry)
+
+  def render_as(self, fp: TextIO, format: str) -> None:
+    self.RENDERERS[format](fp, self)
 
 
 class ChangelogManager:
@@ -95,3 +105,90 @@ class ChangelogManager:
     os.rename(unreleased.filename, self.version(version).filename)
     self._cache.clear()
     return self.version(version)
+
+
+def _group_entries_by_component(entries):
+  key = lambda x: x.components[0]
+  return list(Stream.sortby(entries, key).groupby(key, collect=list))
+
+
+def render_changelog_for_terminal(fp: TextIO, changelog: Changelog) -> None:
+  """
+  Renders a #Changelog for the terminal to *fp*.
+  """
+
+  def _md_term_stylize(text: str) -> str:
+    def _code(m):
+      return colored(m.group(1), 'cyan')
+    def _issue_ref(m):
+      return colored(m.group(0), 'yellow', attrs=['bold'])
+    text = re.sub(r'`([^`]+)`', _code, text)
+    text = re.sub(r'#\d+', _issue_ref, text)
+    return text
+
+  def _fmt_issue(i):
+    if str(i).isdigit():
+      return '#' + str(i)
+    return i
+
+  def _fmt_issues(entry):
+    if not entry.issues:
+      return None
+    return '(' + ', '.join(colored(_fmt_issue(i), 'yellow', attrs=['underline']) for i in entry.issues) + ')'
+
+  def _fmt_types(entry):
+    return ', '.join(colored(f, attrs=['bold']) for f in entry.types)
+
+  def _fmt_components(entry):
+    if len(entry.components) <= 1:
+      return None
+    return '(' + ', '.join(colored(f, 'red', attrs=['bold', 'underline']) for f in entry.components[1:]) + ')'
+
+  if hasattr(shutil, 'get_terminal_size'):
+    width = shutil.get_terminal_size((80, 23))[0]
+  else:
+    width = 80
+
+  # Explode entries by component.
+  for component, entries in _group_entries_by_component(changelog.entries):
+    maxw = max(len(', '.join(x.types)) for x in entries)
+    fp.write(colored(component or 'No Component', 'red', attrs=['bold', 'underline']) + '\n')
+    for entry in entries:
+      lines = textwrap.wrap(entry.description, width - (maxw + 4))
+      suffix_fmt = ' '.join(filter(bool, (_fmt_issues(entry), _fmt_components(entry))))
+      lines[-1] += ' ' + suffix_fmt
+      delta = maxw - len(', '.join(entry.types))
+      fp.write('  {} {}\n'.format(colored((_fmt_types(entry) + ':') + ' ' * delta, attrs=['bold']), _md_term_stylize(lines[0])))
+      for line in lines[1:]:
+        fp.write('  {}{}\n'.format(' ' * (maxw+2), _md_term_stylize(line)))
+
+
+def render_changelog_as_markdown(fp: TextIO, changelog: Changelog) -> None:
+
+  def _fmt_issue(i):
+    if str(i).isdigit():
+      return '#' + str(i)
+    return i
+
+  def _fmt_issues(entry):
+    if not entry.issues:
+      return None
+    return '(' + ', '.join(_fmt_issue(i) for i in entry.issues) + ')'
+
+  fp.write('## {}\n\n'.format(changelog.version or 'unreleased'))
+  for component, entries in _group_entries_by_component(changelog.entries):
+    fp.write('* __{}__\n'.format(component))
+    for entry in entries:
+      description ='**' + ', '.join(entry.types) + '**: ' + entry.description
+      if entry.issues:
+        description += ' ' + _fmt_issues(entry)
+      lines = textwrap.wrap(description, 80)
+      fp.write('  * {}\n'.format(lines[0]))
+      for line in lines[1:]:
+        fp.write('    {}\n'.format(line))
+
+
+Changelog.RENDERERS.update({
+  'terminal': render_changelog_for_terminal,
+  'markdown': render_changelog_as_markdown,
+})
