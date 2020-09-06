35d6c29e88a5755259af3bcecd5caf1cff49e8d3
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-09-06 13:09:45 +0200

    support extras in Requirement, rename VendoredRequirement.value to .location and change semantics

---
+++ src/test/test_requirements.py
@@ -0,0 +1,39 @@
+
+from pytest import raises
+
+from shut.model.requirements import Requirement, VendoredRequirement, VersionSelector
+
+
+def test_parse_requirement():
+  assert Requirement.parse('python') == Requirement('python')
+  assert Requirement.parse('python') == Requirement('python', VersionSelector.ANY)
+  assert Requirement.parse('python 3.4') == Requirement('python', VersionSelector('3.4'))
+  assert Requirement.parse('python ^2.7|^3.4') == Requirement('python', VersionSelector('^2.7|^3.4'))
+
+  assert Requirement.parse('mypackage[a,b,c]') == Requirement('mypackage', VersionSelector.ANY, ['a', 'b', 'c'])
+  assert Requirement.parse('mypackage[a ,b,c]') == Requirement('mypackage', VersionSelector.ANY, ['a', 'b', 'c'])
+  assert Requirement.parse('mypackage[c,a,b]') == Requirement('mypackage', VersionSelector.ANY, ['a', 'b', 'c'])
+  assert Requirement.parse('mypackage[c,a,b] ~0.4.2') == Requirement('mypackage', VersionSelector('~0.4.2'), ['a', 'b', 'c'])
+
+  with raises(ValueError):
+    Requirement.parse('mypackage[]')
+  with raises(ValueError):
+    Requirement.parse('mypackage[] ~0.4.2')
+  with raises(ValueError):
+    Requirement.parse('vendored/liba')
+
+
+def test_parse_vendored_requirememt():
+  assert VendoredRequirement.parse('./vendored/liba') == \
+    VendoredRequirement(VendoredRequirement.Type.Path, './vendored/liba')
+  assert VendoredRequirement.parse('vendored/liba', fallback_to_path=True) == \
+    VendoredRequirement(VendoredRequirement.Type.Path, './vendored/liba')
+  assert VendoredRequirement.parse('git+https://github.com/a/b.git') == \
+    VendoredRequirement(VendoredRequirement.Type.Git, 'https://github.com/a/b.git')
+
+  with raises(ValueError):
+    VendoredRequirement.parse('vendored/liba')
+  with raises(ValueError):
+    VendoredRequirement.parse('git+without_url')
+  with raises(ValueError):
+    VendoredRequirement.parse('hg+https://mercurial.org/a/b.hg')
