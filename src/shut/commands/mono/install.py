f7b6c471a8be818f34342583e55e283e6fa4700b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-09-08 14:49:41 +0200

    fix: `shut mono install` now uses topological order to retrieve package requirements

--- src/shut/commands/mono/install.py
+++ src/shut/commands/mono/install.py
@@ -1,7 +1,8 @@
 
 import click
+import networkx as nx
 
-from shut.commands.pkg.install import collect_installable_requirements, run_install
+from shut.commands.pkg.install import collect_requirements, run_install, split_extras
 from shut.model.monorepo import MonorepoModel
 from shut.model.requirements import RequirementsList
 from . import mono, project
@@ -10,7 +11,7 @@ from . import mono, project
 @mono.command()
 @click.option('--develop/--no-develop', default=True,
   help='Install in develop mode (default: true)')
-@click.option('--extra', help='Specify one or more extras to install.')
+@click.option('--extra', type=split_extras, help='Specify one or more extras to install.')
 @click.option('-U', '--upgrade', is_flag=True, help='Upgrade all packages (forwarded to pip install).')
 @click.option('-q', '--quiet', is_flag=True, help='Quiet install')
 @click.option('--pip', help='Override the command to run Pip. Defaults to "python -m pip" or the PIP variable.')
@@ -23,11 +24,13 @@ def install(develop, extra, upgrade, quiet, pip, pip_args, dry):
   The command used to invoke Pip can be overwritten using the `PIP` environment variable.
   """
 
-  project.load_or_exit(expect=MonorepoModel)
-  extra = set((extra or '').split(','))
+  monorepo = project.load_or_exit(expect=MonorepoModel)
+  graph = monorepo.get_inter_dependencies_graph()
+  package_map = {p.name: p for p in project.packages}
 
   reqs = []
-  for package in project.packages:
-    reqs.append((package.get_directory(), collect_installable_requirements(package, False, extra)))
+  for package_name in nx.algorithms.dag.topological_sort(graph):
+    package = package_map[package_name]
+    reqs.append((package.get_directory(), collect_requirements(package, False, extra)))
 
-  run_install(pip, reqs, develop, extra,upgrade, quiet, dry, pip_args)
+  run_install(pip, reqs, develop, upgrade, quiet, dry, pip_args)
