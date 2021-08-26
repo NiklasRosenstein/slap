2b3a000f65ffb93b25850d95a450aecd6790d414
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-26 15:52:46 +0200

    feature: Shut now recognizes lowercase readme/license files
    fixes #31

--- src/shut/utils/fs.py
+++ src/shut/utils/fs.py
@@ -3,21 +3,28 @@ import os
 import typing as t
 
 
-def get_file_in_directory(directory: str, prefix: str, preferred: t.List[str]) -> t.Optional[str]:
+def get_file_in_directory(
+  directory: str,
+  prefix: str,
+  preferred: t.List[str],
+  case_sensitive: bool = True,
+) -> t.Optional[str]:
   """
   Returns a file in *directory* that is either in the *preferred* list or starts with
   specified *prefix*.
   """
 
+  if not case_sensitive:
+    preferred = [x.lower() for x in preferred]
+
   choices = []
   for name in sorted(os.listdir(directory)):
-    if name in preferred:
-      break
+    if (case_sensitive and name in preferred) or (not case_sensitive and name.lower() in preferred):
+      return os.path.join(directory, name)
     if name.startswith(prefix):
       choices.append(name)
   else:
     if choices:
       return choices[0]
-    return None
 
-  return os.path.join(directory, name)
+  return None
