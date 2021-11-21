517bded2ff54306257d5622a08a1ba1ec967ffe5
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-11-21 19:01:44 +0900

    (shut) bump version to 0.21.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ extras_require['test'] = test_requirements
 
 setuptools.setup(
   name = 'shut',
-  version = '0.21.0',
+  version = '0.21.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Automates the heavy lifting of release and distribution management for pure Python packages.',
