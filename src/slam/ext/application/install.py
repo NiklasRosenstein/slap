ddebc30595acf75e8a36bb69872caa19c45e6d9f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-16 23:23:59 +0100

    add `--only` option to `slam install`

--- src/slam/ext/application/install.py
+++ src/slam/ext/application/install.py
@@ -1,9 +1,11 @@
 
 import os
+from pathlib import Path
 import subprocess as sp
 
 from slam.application import Application, Command, option
 from slam.plugins import ApplicationPlugin
+from slam.project import Project
 from slam.util.python import Environment
 
 venv_check_option = option(
@@ -29,24 +31,30 @@ class InstallCommandPlugin(Command, ApplicationPlugin):
   name = "install"
   options = [
     option(
-      "--link",
+      "only",
+      description="Path to the subproject to install only. May still cause other projects to be installed if "
+        "required by the selected project via inter dependencies, but only their run dependencies will be installed.",
+      flag=False,
+    ),
+    option(
+      "link",
       description="Symlink the root project using <opt>slam link</opt> instead of installing it directly.",
     ),
     option(
-      "--no-dev",
+      "no-dev",
       description="Do not install development dependencies.",
     ),
     option(
-      "--no-root",
+      "no-root",
       description="Do not install the package itself, but only its dependencies.",
     ),
     venv_check_option,
     option(
-      "--python", "-p",
+      "python", "p",
       description="The Python executable to install to.",
       flag=False,
       default=os.getenv('PYTHON', 'python'),
-    )
+    ),
   ]
 
   def load_configuration(self, app: Application) -> None:
@@ -60,19 +68,40 @@ class InstallCommandPlugin(Command, ApplicationPlugin):
     if not venv_check(self):
       return 1
 
+    if only_project := self.option("only"):
+      project_path = Path(only_project).resolve()
+      projects = [p for p in self.app.projects if p.directory.resolve() == project_path]
+      if not projects:
+        self.line_error(f'error: "{only_project}" does not point to a project', 'error')
+        return 1
+      assert len(projects) == 1, projects
+      project_dependencies = self._get_project_dependencies(projects[0])
+    else:
+      projects = self.app.get_projects_in_topological_order()
+      project_dependencies = []
+
     dependencies = []
-    for project in self.app.get_projects_in_topological_order():
+    for project in projects + project_dependencies:
       if not self.option("no-root") and not self.option("link"):
         dependencies.append(str(project.directory.resolve()))
-
       dependencies += project.dependencies().run
+    for project in projects:
       if not self.option("no-dev"):
         dependencies += project.dependencies().dev
 
-    if (res := sp.call([self.option("python"), "-m", "pip", "install"] + dependencies)) != 0:
+    pip_command = [self.option("python"), "-m", "pip", "install"] + dependencies
+    if self.option("quiet"):
+      pip_command += ['-q']
+    if (res := sp.call(pip_command)) != 0:
       return res
 
     if self.option("link"):
       self.call("link")
 
     return 0
+
+  def _get_project_dependencies(self, project: Project) -> list[Project]:
+    dependencies = project.get_interdependencies(self.app.projects)
+    for dep in dependencies[:]:
+      dependencies = self._get_project_dependencies(dep) + dependencies
+    return dependencies
