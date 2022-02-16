01ecf46ab78b3be5cf3dd9374b65298e0f3c64bb
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-16 18:08:28 +0100

    add `slam install` command

---
+++ src/slam/ext/application/install.py
@@ -0,0 +1,62 @@
+
+import os
+import subprocess as sp
+from slam.application import Application, Command, option
+from slam.plugins import ApplicationPlugin
+
+
+class InstallCommandPlugin(Command, ApplicationPlugin):
+  """ Install your project and its dependencies via Pip. """
+
+  app: Application
+  name = "install"
+  options = [
+    option(
+      "--link",
+      description="Symlink the root project using <opt>slam link</opt> instead of installing it directly.",
+    ),
+    option(
+      "--no-dev",
+      description="Do not install development dependencies.",
+    ),
+    option(
+      "--no-root",
+      description="Do not install the package itself, but only its dependencies.",
+    ),
+    option(
+      "--no-venv-check",
+      description="Do not check if the target Python environment is a virtual environment.",
+    ),
+    option(
+      "--python", "-p",
+      description="The Python executable to install to.",
+      flag=False,
+      default=os.getenv('PYTHON', 'python'),
+    )
+  ]
+
+  def load_configuration(self, app: Application) -> None:
+    return None
+
+  def activate(self, app: Application, config: None) -> None:
+    self.app = app
+    app.cleo.add(self)
+
+  def handle(self) -> int:
+    dependencies = []
+
+    # TODO: venv check
+    # TODO: check for dependencies between projects to ensure right install order
+
+    for project in self.app.projects:
+      if not self.option("no-root") and not self.option("link"):
+        dependencies.append(str(project.directory.resolve()))
+
+      dependencies += project.dependencies().run
+      if not self.option("no-dev"):
+        dependencies += project.dependencies().dev
+
+    sp.call([self.option("python"), "-m", "pip", "install"] + dependencies)
+
+    if self.option("link"):
+      self.call("link")
