0a285bb62cf4144aa0075a11d31589ee96c45f78
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-30 16:55:29 +0200

    improvement: Improve changelog checks plugin to catch more types of errors to return in the check details

--- src/slap/ext/checks/changelog.py
+++ src/slap/ext/checks/changelog.py
@@ -2,7 +2,7 @@
 import dataclasses
 import typing as t
 
-from slap.check import Check
+from slap.check import Check, CheckResult, check, get_checks
 from slap.ext.application.changelog import get_changelog_manager
 from slap.plugins import CheckPlugin
 from slap.project import Project
@@ -10,32 +10,40 @@ from slap.project import Project
 
 @dataclasses.dataclass
 class ChangelogValidationCheckPlugin(CheckPlugin):
+  """ This check plugin validates the structured changelog files, if any.
+
+  Plugin ID: `changelog` """
 
   def get_project_checks(self, project: Project) -> t.Iterable[Check]:
-    self.manager = get_changelog_manager(project.repository, project)
-    yield self._check_changelogs()
+    return get_checks(self, project)
 
-  def _check_changelogs(self) -> Check:
+  @check('validate')
+  def _validate_changelogs(self, project: Project) -> tuple[CheckResult, str | None]:
+    import tomli
     from databind.core.converter import ConversionError
 
+    manager = get_changelog_manager(project.repository, project)
+    bad_files = []
     bad_changelogs = []
     count = 0
-    for changelog in self.manager.all():
+    for changelog in manager.all():
       count += 1
       try:
         for entry in changelog.load().entries:
-          self.manager.validate_entry(entry)
-      except (ConversionError, ValueError) as exc:
-        bad_changelogs.append((changelog.path.name, str(exc), entry.id))
+          try:
+            manager.validate_entry(entry)
+          except (ConversionError, ValueError) as exc:
+            bad_changelogs.append((changelog.path.name, str(exc), entry.id))
+      except (tomli.TOMLDecodeError, ConversionError) as exc:
+        bad_files.append((changelog.path.name, str(exc)))
 
-    check_name = 'validate'
     if not count:
-      return Check(check_name, Check.Result.SKIPPED, None)
+      return CheckResult.SKIPPED, None
 
-    return Check(
-      check_name,
+    return (
       Check.ERROR if bad_changelogs else Check.Result.OK,
       f'Broken or invalid changelogs' if bad_changelogs else
         f'All {count} changelogs are valid.',
+      '\n'.join(f'<i>{fn}</i>: {err}' for fn, err in bad_files) if bad_files else None + '\n' +
       '\n'.join(f'<i>{fn}</i>: id=<fg=yellow>"{entry_id}"</fg>: {err}' for fn, err, entry_id in bad_changelogs) if bad_changelogs else None,
     )
