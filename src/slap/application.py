22dbc5e1966dc1e65ebc601fe01558003b965ea4
Alex Spencer <alex.spencer@helsing.ai>
2023-09-27 14:49:12 +0100

    Hide git fatal error if not in a git dir

--- src/slap/application.py
+++ src/slap/application.py
@@ -304,7 +304,7 @@ def find_repository(directory: Path) -> Repository:
     directory = directory.resolve()
 
     try:
-        git_root = Path(sp.check_output(["git", "rev-parse", "--show-toplevel"], cwd=directory).decode().strip())
+        git_root = Path(sp.check_output(["git", "rev-parse", "--show-toplevel"], cwd=directory, stderr=sp.STDOUT).decode().strip())
     except sp.CalledProcessError:
         git_root = None
 
